69f8311f6869ebf11ee8cef0b1cd20c11faa8164
Henrik Sandklef <hesa@sandklef.com>
2014-02-11 18:14:18 +0100

    Added notes on purpose

---
+++ com/sandklef/edu/ConsoleMenu/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Simple package to ease up console menus
+ *
+ * The main purpose is, rather thanease up writing a console menu, to
+ * show the use of Interfaces. In the examples we show how to create
+ * and add MenuItem classes and also how to add anonymous
+ * classes/objects.
+ *
+ * See package com.sandklef.edu.examples for examples on how to use it.
+ */
+
+package com.sandklef.edu.ConsoleMenu;
